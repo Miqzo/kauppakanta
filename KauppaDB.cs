1ee70d9b6801859dd940e15472a96733b40b9441
Miqzo <mvainamo@gmail.com>
2025-09-10 09:12:23 +0300

    Fixed possible crash

--- KauppaDB.cs
+++ KauppaDB.cs
@@ -10,54 +10,62 @@ public class KauppaDB
     public KauppaDB()
     {
         // Luodaan yhteys tietokantaan
-        var connection = new SqliteConnection(_connectionString);
-        connection.Open();
-        // Luodaan taulut, jos niitä ei vielä ole
-        // Yksinkertainen tietokanta, jossa on yksi taulu
-        // Taulu Tuotteet, sarakkeet id, nimi, hinta
-        var commandForTableCreation = connection.CreateCommand();
-        commandForTableCreation.CommandText =
-            "CREATE TABLE IF NOT EXISTS Tuotteet (id INTEGER PRIMARY KEY, nimi TEXT, hinta REAL)";
-        commandForTableCreation.ExecuteNonQuery();
-        connection.Close();
+        using (var connection = new SqliteConnection(_connectionString))
+        {
+            connection.Open();
+            // Luodaan taulut, jos niitä ei vielä ole
+            // Yksinkertainen tietokanta, jossa on yksi taulu
+            // Taulu Tuotteet, sarakkeet id, nimi, hinta
+            var commandForTableCreation = connection.CreateCommand();
+            commandForTableCreation.CommandText =
+                "CREATE TABLE IF NOT EXISTS Tuotteet (id INTEGER PRIMARY KEY, nimi TEXT, hinta REAL)";
+            commandForTableCreation.ExecuteNonQuery();
+            // connection.Close();
+        }
     }
 
     public void LisaaTuote(string nimi, double hinta)
     {
-        var connection = new SqliteConnection(_connectionString);
-        connection.Open();
-        // Lisätään tuote tietokantaan
-        var commandForInsert = connection.CreateCommand();
-        commandForInsert.CommandText =
-            "INSERT INTO Tuotteet (nimi, hinta) VALUES (@Nimi, @Hinta)";
-        commandForInsert.Parameters.AddWithValue(@"Nimi", nimi);
-        commandForInsert.Parameters.AddWithValue(@"Hinta", hinta);
-        commandForInsert.ExecuteNonQuery();
-        connection.Close();
+        using (var connection = new SqliteConnection(_connectionString))
+        {
+            connection.Open();
+            // Lisätään tuote tietokantaan
+            var commandForInsert = connection.CreateCommand();
+            commandForInsert.CommandText =
+                "INSERT INTO Tuotteet (nimi, hinta) VALUES (@Nimi, @Hinta)";
+            commandForInsert.Parameters.AddWithValue(@"Nimi", nimi);
+            commandForInsert.Parameters.AddWithValue(@"Hinta", hinta);
+            commandForInsert.ExecuteNonQuery();
+            // connection.Close();
+        }
     }
     public string HaeTuotteet(string haettuNimi)
     {
-        var connection = new SqliteConnection(_connectionString);
-        connection.Open();
-        var commandForSelect = connection.CreateCommand();
-        commandForSelect.CommandText =
-            "SELECT * FROM Tuotteet WHERE nimi LIKE @Nimi";
-        commandForSelect.Parameters.AddWithValue(@"Nimi", haettuNimi);
-        var reader = commandForSelect.ExecuteReader();
-        string tuotteet = "";
-        while (reader.Read())
+        using (var connection = new SqliteConnection(_connectionString))
         {
-            tuotteet += $"Id: {reader.GetInt32(0)}, Nimi: {reader.GetString(1)}, Hinta: {reader.GetDouble(2)}";
-        }
-        reader.Close();
-        connection.Close();
-        if (tuotteet == "")
-        {
-            return "Hakemaasi tuotetta ei löytynyt.";
-        }
-        else
-        {
-            return tuotteet;
+            connection.Open();
+            var commandForSelect = connection.CreateCommand();
+            commandForSelect.CommandText =
+                "SELECT * FROM Tuotteet WHERE nimi LIKE @Nimi";
+            commandForSelect.Parameters.AddWithValue(@"Nimi", haettuNimi);
+            using (var reader = commandForSelect.ExecuteReader())
+            {
+                string tuotteet = "";
+                while (reader.Read())
+                {
+                    tuotteet += $"Id: {reader.GetInt32(0)}, Nimi: {reader.GetString(1)}, Hinta: {reader.GetDouble(2)}";
+                }
+                reader.Close();
+                connection.Close();
+                if (tuotteet == "")
+                {
+                    return "Hakemaasi tuotetta ei löytynyt.";
+                }
+                else
+                {
+                    return tuotteet;
+                }
+            }
         }
     }
 }
